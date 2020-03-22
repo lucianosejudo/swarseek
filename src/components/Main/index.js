4c34efdf223273d089f5bc9e68e4ff8e15a7f798
Luciano Sejudo <luciano.sejudo@gmail.com>
2020-03-22 00:55:45 -0300

    add material ui

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import Grid from '@material-ui/core/Grid';
 import SpaceBackground from 'components/SpaceBackground'
 import './styles.scss';
 
 function Main() {
   return (
     <div className="main">
+      <Grid container spacing={5}>
+        <Grid item xs={12} sm={2}>
+        </Grid>
+        <Grid item xs={12} sm={4}>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+        </Grid>
+      </Grid>
       <SpaceBackground />
     </div>
   );
