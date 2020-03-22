c0074de323367f18a8a5ea531144cf5827805093
Luciano Sejudo <luciano.sejudo@gmail.com>
2020-03-22 00:31:37 -0300

    add stas background

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import SpaceBackground from 'components/SpaceBackground'
 import './styles.scss';
 
 function Main() {
   return (
     <div className="main">
-      
+      <SpaceBackground />
     </div>
   );
 }
---
+++ src/components/SpaceBackground/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import './styles.scss'
+
+export default function SpaceBackground() {
+  return (
+  	<div class="stars"></div>
+  )
+}
