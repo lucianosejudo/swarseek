cbd41a52e9de5f55460ba125049294d3349d509a
Luciano Sejudo <luciano.sejudo@gmail.com>
2020-03-22 22:22:14 -0300

    add logo title

---
+++ src/components/LogoTitle/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import title from 'assets/images/logo-title.png'
+
+export default function LogoTitle() {
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <img src={title} alt="logo" />
+    </div>
+  )
+}
--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,13 +1,17 @@
-import React from 'react';
-import Grid from '@material-ui/core/Grid';
+import React from 'react'
+import Grid from '@material-ui/core/Grid'
 import Search from 'components/Search'
+import LogoTitle from 'components/LogoTitle'
 import Menu from 'components/Menu'
-import './styles.scss';
+import './styles.scss'
 
 function Main() {
   return (
     <div className="main">
       <Grid container spacing={5}>
+        <Grid item sm={12}>
+          <LogoTitle />
+        </Grid>
         <Grid item xs={12} sm={4}>
           <Menu />
         </Grid>
