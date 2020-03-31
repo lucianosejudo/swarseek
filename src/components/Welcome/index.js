31f518ab5a70a6aeb04d11326a6eef78e233ad93
Luciano Sejudo <luciano.sejudo@gmail.com>
2020-03-31 20:28:52 -0300

    add welcome images

--- src/components/Welcome/index.js
+++ src/components/Welcome/index.js
@@ -1,12 +1,20 @@
 import React from 'react'
+import chewbacca from 'assets/images/chewbacca.png'
+import vader from 'assets/images/vader.png'
+import leia from 'assets/images/leia.png'
 import './styles.scss'
 
 export default function Welcome() {
   return (
     <div className="welcome">
       <div className="welcome__title">¡Bienvenido a Swarseek!</div>
-      <p className="welcome__label">Busque informacion de sus personajes o peliculas favoritas de Star Wars y</p>
-      <p className="welcome__msg">QUE LA FUERZA ESTE CONTIGO... SIEMPRE</p>
+      <p className="welcome__label">Busque informacion sobre sus personajes o peliculas favoritas de Star Wars y que...</p>
+      <p className="welcome__msg">...LA FUERZA ESTE CONTIGO, SIEMPRE</p>
+      <div className="welcome__images">
+          <img src={vader} alt="vader" />
+          <img src={chewbacca} alt="chewbacca" /> 
+          <img src={leia} alt="leia" />
+      </div>
     </div>
   )
 }
