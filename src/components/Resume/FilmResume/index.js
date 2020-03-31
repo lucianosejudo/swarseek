2c12f6c733b57c7cd313c331f569aef9fcc2a01a
Luciano Sejudo <luciano.sejudo@gmail.com>
2020-03-31 20:11:11 -0300

    handle film resume styles

--- src/components/Resume/FilmResume/index.js
+++ src/components/Resume/FilmResume/index.js
@@ -4,13 +4,19 @@ import './styles.scss'
 
 export default function FilmResume({ film }) {
   return (
-    <div>
+    <div className="film-resume">
       <div className="film-resume__title">{film.title}</div>
-      <div className="film-resume__director">Director: {film.director}</div>
-      <div className="film-resume__img">
-        <img src={getImageFromUrl(film.url, 'films')} alt={film.title} />
+      <div className="film-resume__data">
+        <div className="film-resume__data-img">
+          <img src={getImageFromUrl(film.url, 'films')} alt={film.title} />
+        </div>
+        <div>
+          <div className="film-resume__data-director">Director: {film.director}</div>
+          <div className="film-resume__data-producer">Productores: {film.producer}</div>
+          <div className="film-resume__data-release">Fecha de estreno: {film.release_date}</div>
+          <div className="film-resume__data-resume">{film.opening_crawl}</div>
+        </div>
       </div>
-      <div className="film-resume__resumen">{film.opening_crawl}</div>
     </div>
   )
 }
